df97b411c0990404c9864161e5487d182f39bda8
leoh7 <leoh7@naver.com>
2019-03-21 18:34:14 +0900

    gulp를 사용, 바벨을 쓰기위한 의존성 추가

--- learning-js/gulpfile.js
+++ learning-js/gulpfile.js
@@ -1,5 +1,7 @@
-const gulp = require('gulp');
 // 걸프 의존성을 여기 쓴다
+const gulp = require('gulp');
+// 바벨을 걸프와 함께 사용하기
+const babel = require('gulp-babel');
 
 gulp.task('default', function() {
 	// 걸프 작업을 여기 쓴다
