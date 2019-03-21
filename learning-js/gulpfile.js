5d04bb7d877e5d44a7d9e1259687bc828ec4adcc
leoh7 <leoh7@naver.com>
2019-03-21 21:00:12 +0900

    [ESLint] gulpfile에 gulp-eslint 를 사용하기 위한 설정

--- learning-js/gulpfile.js
+++ learning-js/gulpfile.js
@@ -1,10 +1,17 @@
 // 걸프 의존성을 여기 쓴다
 const gulp = require('gulp');
-// 바벨을 걸프와 함께 사용하기
+// babel을 gulp와 함께 사용하기
 const babel = require('gulp-babel');
+// ESLint를 gulp와 함께 사용
+const eslint = require('gulp-eslint');
 
 gulp.task('default', function() {
 	// 걸프 작업을 여기 쓴다
+
+	// ESLint 실행
+	gulp.src(["es6/**/*.js", "public/es6/**/*.js"])
+		.pipe(eslint())
+		.pipe(eslint.format());
 
 	// 노드 소스
 	gulp.src("es6/**/*.js")	// **는 서브디렉터리를 포함한 모든 디렉토리
