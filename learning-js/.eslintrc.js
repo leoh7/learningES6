a929065fabe43e13b63393c8e85b0b31b1c863b8
leoh7 <leoh7@naver.com>
2019-03-21 21:41:36 +0900

    [ESLint] airbnb-base 가 너무 까다로워 바꿔줌

--- learning-js/.eslintrc.js
+++ learning-js/.eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
         "es6": true,
         "node": true
     },
-    "extends": "airbnb-base",
+    "extends": "eslint:recommended",
     "globals": {
         "Atomics": "readonly",
         "SharedArrayBuffer": "readonly"
