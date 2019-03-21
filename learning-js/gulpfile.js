5991fa21ac6a684e5aef2d6023c7814752cddff1
leoh7 <leoh7@naver.com>
2019-03-21 18:34:59 +0900

    서버 소스(노드 소스)와 클라이언트 소스(브라우저 소스)를 babel 로 처리하기 위한 작업 내용 추가

--- learning-js/gulpfile.js
+++ learning-js/gulpfile.js
@@ -5,4 +5,13 @@ const babel = require('gulp-babel');
 
 gulp.task('default', function() {
 	// 걸프 작업을 여기 쓴다
+
+	// 노드 소스
+	gulp.src("es6/**/*.js")	// **는 서브디렉터리를 포함한 모든 디렉토리
+		.pipe(babel())				// 선택한 파일들을 바벨에 연결(pipe)
+		.pipe(gulp.dest("dist"));	// 연결된 파일들을 gulp로 컴파일해 dist 디렉터리에 저장
+	// 브라우저 소스
+	gulp.src("public/es6/**/*.js")
+		.pipe(babel())
+		.pipe(gulp.dest("public/dist"));
 });
